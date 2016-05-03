54e0b73a7e4d03abb3bf10c8a8f7507ffefd2ef5
RylonMcknz <rmmelendez93@gmail.com>
2016-05-03 10:17:07 -0400

    Added commentation about RegEx codes.

--- FirstRegEx/FirstRegEx/FirstRegEx.cpp
+++ FirstRegEx/FirstRegEx/FirstRegEx.cpp
@@ -8,7 +8,34 @@ using namespace std;
 
 int main()
 {
+	/*
+
+	\d		any number
+	\D		anything but a number
+	\s		any space
+	\S		anything but a space
+	\w		any character
+	\W		anything but a character
+	.		any character other than endline
+	\b		any space that preceeds or(maybe and) follows a word
+
+	RegEx to gather addresses of the format 123 Main St.
+
+	\d{1,5}			
+	\s			    
+	\w+
+	\s
+	\w+
+	\.
+	
+	OR
+
+	\d{1,5}\s\w+\s\w+\.
+
+	*/
 	cout << "Hello world." << endl;
+	cout << "Hello again." << endl;
+	
     return 0;
 }
 
